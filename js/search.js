b245f17a6e6ce989620be8a760c51a18fc57aaf3
teo gkouvas <thodoris.gk@hotmail.gr>
2019-11-26 23:30:19 +0200

    5 day forecast includes weather data every 3 hours

--- js/search.js
+++ js/search.js
@@ -4,11 +4,15 @@ function App($) {
 
     const input = $("#input");
     const output = $("#output");
-    const table = $("#dataTable");
+    const dataTableWithDates = $("#dataTableWithDates");
+    const dataTableWithDateForecastEvery3Hours = $("#dataTableWithDateForecastEvery3Hours");
     const forecastForCity = $("#forecastForCity");
+    const backButton = $('#backButton');
+    let weatherData;
 
     input.focus();
     input.on("input", handleInput);
+    backButton.click(getForecast);
 
     function handleInput() {
         const inputLength = input.val().length;
@@ -18,47 +22,75 @@ function App($) {
         } else if (inputLength < 2) { // Handling when user is deleting and reaching input length 1 and below
             clearTimeout(window.timer);
             output.animate({ opacity: 0 }, 1000);
-            $("#forecastForCity").animate({ opacity: 0 }, 1000);
-            table.empty();
+            forecastForCity.animate({ opacity: 0 }, 1000);
+            dataTableWithDates.empty();
         }
     }
 
     function getForecast() {
+        backButton.css('opacity', '0');
+        dataTableWithDateForecastEvery3Hours.empty();
+
         let URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input.val() + "&units=metric" + "&appid=f60f25502d741d7b0dc7d58de36d5ea7";
         let options = {
             url: URL,
-            success: handleResponce
+            success: show5dayForecast
         };
         $.ajax(options);
     }
 
-    function handleResponce(data) {
-        table.empty();
-
+    function show5dayForecast(data) {
+        weatherData = data;
+        dataTableWithDates.empty();
         let dates = getDates(data);
 
-        var city = '<h2>Weather forecast for ' + data.city.name + ', ' + data.city.country + '</h2>'
+        const city = '<h2>Weather forecast for ' + data.city.name + ', ' + data.city.country + '</h2>'
         forecastForCity.html(city);
 
         for (i = 0; i < dates.length; i++) {
             const dateDetails = getDetailsForDate(data.list, dates[i]);
 
-            let columnDate = "<td class='selectedDate'>" + dateDetails[0].dt_txt.substring(0, 10) + "</td>";
-            let columnDescription = "<td class='selectedDate'>" + capitalizeFirstLetter(dateDetails[0].weather[0].description) + "<img src='http://openweathermap.org/img/w/" + dateDetails[0].weather[0].icon + ".png'> " + "</td>";
-            let columnTemperature = "<td class='selectedDate'>" + dateDetails[0].main.temp + " &deg;C" + "</td>";
-            let columnHumidity = "<td class='selectedDate'>" + dateDetails[0].main.humidity + "%" + "</td>";
-            let columnMinTemperature = "<td class='selectedDate'>" + dateDetails[0].main.temp_min + "&deg;C" + "</td>";
-            let columnMaxTemperature = "<td class='selectedDate'>" + dateDetails[0].main.temp_max + "&deg;C" + "</td>";
-            let columnWindSpeed = "<td class='selectedDate'>" + dateDetails[0].wind.speed + " m/s" + "</td>";
-
-            let newRowContent = "<tr id='" + dateDetails[0].dt_txt.substring(0, 10) + "'>" + columnDate + columnDescription + columnTemperature + columnHumidity + columnMinTemperature + columnMaxTemperature + columnWindSpeed + "</tr>";
-
-            table.append(newRowContent);
-
+            populateTableWith(dateDetails[0], true);
         }
+        $('tr').click(showDateDetails);
 
-        output.animate({ opacity: 1 }, 50);
         forecastForCity.animate({ opacity: 1 }, 50);
+        output.animate({ opacity: 1 }, 50);
+
+    }
+    function showWeatherDataEvery3Hours() {
+        const selectedDate = sessionStorage.getItem('dateId');
+        dataTableWithDates.empty();
+
+        const city = '<h2>Weather forecast for ' + weatherData.city.name + ', ' + weatherData.city.country + '</h2>'
+        forecastForCity.html(city);
+
+        const dateDetails = getDetailsForDate(weatherData.list, selectedDate);
+        for (let i = 0; i < dateDetails.length; i++) {
+            populateTableWith(dateDetails[i], false);
+        }
+        forecastForCity.animate({ opacity: 1 }, 50);
+        output.animate({ opacity: 1 }, 50);
+        backButton.animate({ opacity: 1 }, 50);
+    }
+    //Utils
+
+    function populateTableWith(details, showOnlyDate) {
+        let dateTxt;
+        if (showOnlyDate) {
+            dateTxt = details.dt_txt.substring(0, 10);
+        } else {
+            dateTxt = details.dt_txt;
+        }
+        let columnDate = "<td>" + dateTxt + "</td>";
+        let columnDescription = "<td>" + capitalizeFirstLetter(details.weather[0].description) + "<img src='http://openweathermap.org/img/w/" + details.weather[0].icon + ".png'> " + "</td>";
+        let columnTemperature = "<td>" + details.main.temp + " &deg;C" + "</td>";
+        let columnHumidity = "<td>" + details.main.humidity + "%" + "</td>";
+        let columnMinTemperature = "<td>" + details.main.temp_min + "&deg;C" + "</td>";
+        let columnMaxTemperature = "<td>" + details.main.temp_max + "&deg;C" + "</td>";
+
+        let newRowContent = "<tr id='" + dateTxt + "'>" + columnDate + columnDescription + columnTemperature + columnHumidity + columnMinTemperature + columnMaxTemperature + "</tr>";
+        dataTableWithDates.append(newRowContent);
     }
 
     function getDates(data) {
@@ -72,7 +104,7 @@ function App($) {
     }
 
     function getDetailsForDate(list, date) {
-      
+
         let dateValues = [];
         for (let i = 0; i < list.length; i++) {
             if (list[i].dt_txt.substring(0, 10) == date) {
@@ -81,7 +113,14 @@ function App($) {
         }
         return dateValues;
     }
-    //Utils
+
+    function showDateDetails(data) {
+        let dateId = $(this).closest('tr').attr('id');
+        sessionStorage.clear();
+        sessionStorage.setItem('dateId', dateId);
+        showWeatherDataEvery3Hours(data);
+    }
+
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
