1ef71bc492cdf657c60e094a936b9bc1ab41ae9c
teo gkouvas <thodoris.gk@hotmail.gr>
2020-05-02 01:56:14 +0300

    UI improvements

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# WeatherForecast
+# Weather Forecast
+
+## Description
+A Front End Application that calls the free [OpenWeatherMap API](https://openweathermap.org/api) and displays the current weather, daily forecasts, and hourly
+forecasts based on the location that you want.
+
+## Features
+
+-  5 day forecast at any location or city
+
+-  Weather data every 3 hours for the selected date
+  
+- Responsive and cater for views from tablets and mobiles
+
+## Displayed info
+ * Location
+ * Temperature
+ * Humidity 
+ * Weather Icon
+ * Description 
+ * Min Temperature
+ * Max Temperature
--- js/search.js
+++ js/search.js
@@ -11,25 +11,25 @@ function App($) {
     let weatherData;
 
     input.focus();
-    input.on("input", handleInput);
+    input.on("keyup", handleInput);
+    input.on("paste", preventDefault);
     backButton.click(getForecast);
 
     function handleInput() {
         const inputLength = input.val().length;
         if (inputLength > 2) {
             clearTimeout(window.timer);
-            window.timer = setTimeout(getForecast, 700); // blank period timeout to prevent wasteful of the server resources
-        } else if (inputLength < 2) { // Handling when user is deleting and reaching input length 1 and below
+            window.timer = setTimeout(getForecast, 600); // blank period timeout to prevent wasteful of the server resources
+        } else if (inputLength <1) {
             clearTimeout(window.timer);
-            output.animate({ opacity: 0 }, 1000);
-            forecastForCity.animate({ opacity: 0 }, 1000);
+            forecastForCity.animate({ opacity: 0 }, 900);
+            output.animate({ opacity: 0 }, 600);
             dataTableWithDates.empty();
             weatherData = null;
         }
     }
 
     function getForecast() {
-        backButton.css('opacity', '0');
         dataTableWithDateForecastEvery3Hours.empty();
 
         let URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input.val() + "&units=metric" + "&appid=f60f25502d741d7b0dc7d58de36d5ea7";
@@ -39,16 +39,17 @@ function App($) {
                 success: show5dayForecast
             };
             $.ajax(options);
-        } else {
+        } else { //When back button pressed
             show5dayForecast(weatherData);
+            backButtonDisplay(false);
         }
-
     }
 
     function show5dayForecast(data) {
         weatherData = data;
         dataTableWithDates.empty();
         let dates = getDates(data);
+        changeCursorTo("pointer");
 
         const city = '<h2>Weather forecast for ' + data.city.name + ', ' + data.city.country + '</h2>'
         forecastForCity.html(city);
@@ -58,7 +59,7 @@ function App($) {
 
             populateTableWith(dateDetails[0], true);
         }
-        $('tr').click(showDateDetails);
+        $('tbody > tr').click(showDateDetails);
 
         forecastForCity.animate({ opacity: 1 }, 50);
         output.animate({ opacity: 1 }, 50);
@@ -68,42 +69,39 @@ function App($) {
         const selectedDate = sessionStorage.getItem('dateId');
         dataTableWithDates.empty();
 
-        const city = '<h2>Weather forecast for ' + weatherData.city.name + ', ' + weatherData.city.country + '</h2>'
-        forecastForCity.html(city);
-
         const dateDetails = getDetailsForDate(weatherData.list, selectedDate);
         for (let i = 0; i < dateDetails.length; i++) {
             populateTableWith(dateDetails[i], false);
         }
         forecastForCity.animate({ opacity: 1 }, 50);
         output.animate({ opacity: 1 }, 50);
-        backButton.animate({ opacity: 1 }, 50);
     }
     //Utils
 
     function populateTableWith(details, showOnlyDate) {
+        const description = capitalizeFirstLetter(details.weather[0].description);
         let dateTxt;
         if (showOnlyDate) {
-            dateTxt = details.dt_txt.substring(0, 10);
+            dateTxt = details.dt_txt.substring(5, 10); //month-day
         } else {
-            dateTxt = details.dt_txt;
+            dateTxt = details.dt_txt.substring(5, 16); //month-day hour:minutes
         }
-        let columnDate = "<td>" + dateTxt + "</td>";
-        let columnDescription = "<td>" + capitalizeFirstLetter(details.weather[0].description) + "<img src='http://openweathermap.org/img/w/" + details.weather[0].icon + ".png'> " + "</td>";
-        let columnTemperature = "<td>" + details.main.temp + " &deg;C" + "</td>";
-        let columnHumidity = "<td>" + details.main.humidity + "%" + "</td>";
-        let columnMinTemperature = "<td>" + details.main.temp_min + "&deg;C" + "</td>";
-        let columnMaxTemperature = "<td>" + details.main.temp_max + "&deg;C" + "</td>";
+        const columnDate = "<td>" + dateTxt + "</td>";
+        const columnDescription = "<td>" + "<img src='http://openweathermap.org/img/w/" + details.weather[0].icon + ".png' class='descriptionImg img-fluid' alt='" + description + "' title='" + description + "'>" + "</td>";
+        const columnTemperature = "<td>" + details.main.temp + " &deg;C" + "</td>";
+        const columnHumidity = "<td>" + details.main.humidity + "%" + "</td>";
+        const columnMinTemperature = "<td>" + details.main.temp_min + "&deg;C" + "</td>";
+        const columnMaxTemperature = "<td>" + details.main.temp_max + "&deg;C" + "</td>";
 
-        let newRowContent = "<tr id='" + dateTxt + "'>" + columnDate + columnDescription + columnTemperature + columnHumidity + columnMinTemperature + columnMaxTemperature + "</tr>";
+        const newRowContent = "<tr id='" + dateTxt + "'>" + columnDate + columnDescription + columnTemperature + columnHumidity + columnMinTemperature + columnMaxTemperature + "</tr>";
         dataTableWithDates.append(newRowContent);
     }
 
     function getDates(data) {
         let dates = [];
         for (i = 0; i < data.list.length; i++) {
-            if (!dates.includes(data.list[i].dt_txt.substring(0, 10))) {
-                dates.push(data.list[i].dt_txt.substring(0, 10));
+            if (!dates.includes(data.list[i].dt_txt.substring(5, 10))) {
+                dates.push(data.list[i].dt_txt.substring(5, 10));
             }
         }
         return dates;
@@ -113,7 +111,7 @@ function App($) {
 
         let dateValues = [];
         for (let i = 0; i < list.length; i++) {
-            if (list[i].dt_txt.substring(0, 10) == date) {
+            if (list[i].dt_txt.substring(5, 10) == date) {
                 dateValues.push(list[i]);
             }
         }
@@ -124,14 +122,30 @@ function App($) {
         let dateId = $(this).closest('tr').attr('id');
         sessionStorage.clear();
         sessionStorage.setItem('dateId', dateId);
+        changeCursorTo("default_");
+        backButtonDisplay(true);
         showWeatherDataEvery3Hours(data);
     }
 
-    $(document).ready(function () {
-        $('#input').bind('paste', function (e) {
-            e.preventDefault();
-        });
-    });
+    function changeCursorTo(cursorType) {
+        if (cursorType === "pointer") {
+            $('tbody').css('cursor', 'pointer');
+        } else {
+            $('tbody').css('cursor', 'default');
+        }
+    }
+
+    function backButtonDisplay(bool) {
+        if (bool) {
+            backButton.delay(60).show(0);
+        } else {
+            backButton.hide();
+        }
+    }
+
+    function preventDefault(e) {
+        e.preventDefault();
+    };
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
